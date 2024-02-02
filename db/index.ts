c7676e6bd46fd0ba3e31aae1840b65265e90974c
YuruBeDeniz <saganak.deniz@gmail.com>
2024-02-02 11:03:01 +0100

    fix/connect to MongoDB successfully

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,26 @@
-const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
 
-const MONGO_URI = process.env.MONGODB_URI
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-mongoose
-    .connect(MONGO_URI)
-    .then((x: typeof mongoose) => {
-        console.log("Connected to Mongo!");
-    })
-    .catch((err: any) => {
-        console.error("Error connecting to mongo: ", err);
-    })
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+    
